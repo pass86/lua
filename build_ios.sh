c5a8e298a219bb6bb4ef42eadbfe53c968f20c95
Jimmy Yuan <pass86@gmail.com>
2020-08-25 23:27:58 +0800

    Set path to build

--- build_ios.sh
+++ build_ios.sh
@@ -2,13 +2,7 @@
 
 set -e
 
-mkdir -p build
-
-cd build
-
 # NOTE: Add CMAKE_SYSTEM_VERSION to enable rpath
-cmake -GXcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_SYSTEM_VERSION=9.3 ..
+cmake -GXcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_SYSTEM_VERSION=9.3 -S . -B build
 
-cmake --build .
-
-cd -
+cmake --build build
